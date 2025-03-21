ad1ec35a81741036ed82cc89eb08cf88618d7214
m1yingm1 <zwj15827393587@163.com>
2025-03-21 18:07:20 +0800

    default

--- chatpt2/town_ws/install/setup.ps1
+++ chatpt2/town_ws/install/setup.ps1
@@ -23,6 +23,7 @@ function _colcon_prefix_chain_powershell_source_script {
 
 # source chained prefixes
 _colcon_prefix_chain_powershell_source_script "/opt/ros/humble\local_setup.ps1"
+_colcon_prefix_chain_powershell_source_script "/home/ying/ros2/chatpt2/town_ws/src/village_wang/install\local_setup.ps1"
 
 # source this prefix
 $env:COLCON_CURRENT_PREFIX=(Split-Path $PSCommandPath -Parent)
